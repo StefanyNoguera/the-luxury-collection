f8e1afa1c07f9806c08fa019283f040fca5ef60a
Stefany Noguera <stefanynoguerar@gmail.com>
2023-04-14 10:52:31 -0500

    fixed small thing navbar, and brand is clickable in the show of a product

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :products, only: [:index, :show] do
-    collection do
-      get :filter_by_category
-    end
-  end
+  resources :products, only: [:index, :show]
 end
