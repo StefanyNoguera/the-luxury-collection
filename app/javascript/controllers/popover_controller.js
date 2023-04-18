60428ff1a92a28a03d450948ed701ca7006aad4d
Stefany Noguera <stefanynoguerar@gmail.com>
2023-04-18 15:23:50 -0500

    linked html page with controller

---
+++ app/javascript/controllers/popover_controller.js
@@ -0,0 +1,9 @@
+import Popover from 'stimulus-popover'
+
+// Connects to data-controller="popover"
+export default class extends Popover {
+  connect() {
+    super.connect();
+    console.log("Popover controller connected");
+  }
+}
