a9a5c471f92677c81af6143ebead46396d316c93
Stefany Noguera <stefanynoguerar@gmail.com>
2023-04-18 17:10:06 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
