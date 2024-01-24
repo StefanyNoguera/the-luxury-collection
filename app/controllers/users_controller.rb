3aa410ecaf86a2075396ceb15a7431f966f36366
Stefany Noguera <stefanynoguerar@gmail.com>
2024-01-24 12:49:15 -0500

    user logout fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,5 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
   get 'users/show'
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  # devise_for :users, controllers: {
+  #   registrations: "users/registrations",
+  # }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  as :user do
+    get 'user/signin' => 'devise/sessions#new', :as => :new_user_session
+    post 'user/signin' => 'devise/sessions#create', :as => :user_session
+    get 'user/signout' => 'devise/sessions#destroy', :as => :destroy_user_session, via: [:delete, :get]
+  end
+  resources :users do
+    resources :sellers, only: [:create, :new, :show]
+  end
+  scope "admin" do
+      devise_for :users, :skip => [:sessions]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
   delete "products/:id", to: "products#destroy", as: :destroy_product
@@ -14,8 +25,5 @@ Rails.application.routes.draw do
     resources :wishlists, only: [:create, :new, :index]
     delete "wishlists/:id", to: "wishlists#destroy", as: :destroy_wishlist
     resources :reviews, only: [:create, :new]
-  end
-  resources :users, only: [:show] do
-    resources :sellers, only: [:create, :new, :show]
   end
 end
